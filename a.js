198bf2e0ab08e9e79817ad0997562a56e4dda61c
jsorxyz <july12311ozx@gmail.com>
2024-06-16 10:39:32 +0700

    Add files via upload

---
+++ a.js
@@ -0,0 +1,6 @@
+var plausibleScript = document.createElement("script");
+        plausibleScript.src = "plausible.js"
+        plausibleScript.setAttribute("asnyc", "");
+        plausibleScript.setAttribute("defer", "");
+        plausibleScript.setAttribute("data-domain", "bio.link");
+        document.head.appendChild(plausibleScript);
